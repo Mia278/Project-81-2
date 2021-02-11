fbf96f2878af1d78b515355b83e787f2963027ba
Mia278 <74997937+Mia278@users.noreply.github.com>
2021-02-10 19:19:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+function getParagraph1() {
+    var inputs = [] ;
+
+for (var i  = 1 ;  i<= 6 ; i++)
+ inputs.push (document.getElementById("para1_input_box_" + i).value) ;
+ inputs.join(". ");
+ document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
+}
+
+
+function getParagraph2() {
+    var inputs2 = [] ;
+
+for (var m  = 1 ;  m<= 6 ; m++)
+ inputs2.push (document.getElementById("para2_input_box_" + m).value) ;
+ inputs2.join(". ");
+ document.getElementById("showParagraph2").innerHTML = inputs2.join(". ")
+}
